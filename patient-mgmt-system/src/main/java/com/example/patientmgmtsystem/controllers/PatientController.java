3fd6d0f0c1dbe6af45a756d19a08ebe582a11817
sanketwakhare <sanketwakhare@gmail.com>
2022-08-02 16:55:18 +0530

    setup files

--- patient-mgmt-system/src/main/java/com/example/patientmgmtsystem/controllers/PatientController.java
+++ patient-mgmt-system/src/main/java/com/example/patientmgmtsystem/controllers/PatientController.java
@@ -8,13 +8,11 @@ import com.example.patientmgmtsystem.models.Patient;
 import com.example.patientmgmtsystem.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PatientController {
 
-    private PatientService patientService;
+    private final PatientService patientService;
 
     @Autowired
     public PatientController(PatientService patientService) {
